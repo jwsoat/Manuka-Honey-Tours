2f8b70d190fe159ad991dfc81ef69f41d0be91f3
Jwsoat <34992188+jwsoat@users.noreply.github.com>
2020-03-05 18:03:04 +1300

    footer fixed

--- footer.php
+++ footer.php
@@ -1,7 +1,7 @@
 </body>
-<div class"footer">
-  <div class"navbar fixed-bottom">
-    <p>©2020 Manuka Beehive Honey Tours  <a href="privacy.html">Privacy Policy</a><a href="tos.html">Terms of Use</a> <div class="fb-like" data-href="https://www.facebook.com/manukahoneytours" data-width="" data-layout="standard" data-action="like" data-size="small" data-share="true"></div></p>
-  </div>
-</div>
+<footer id="sticky-footer" class="py-4 bg-dark text-white-50">
+    <div class="container text-center">
+      <small>©2020 Manuka Beehive Honey Tours  <a href="privacy.html">Privacy Policy</a><a href="tos.html">Terms of Use</a> <div class="fb-like" data-href="https://www.facebook.com/manukahoneytours" data-width="" data-layout="standard" data-action="like" data-size="small" data-share="true"></div></small>
+    </div>
+  </footer>
 </html>
